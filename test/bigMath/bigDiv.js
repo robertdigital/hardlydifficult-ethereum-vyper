67a8707fa949a8cc9f75f39c551a347afa957105
Hardly Difficult <hardlydiff@gmail.com>
2019-09-25 17:25:38 -0700

    Remove useless tests (#52)
    * Remove useless tests
    
    * Update bigDiv.js

--- test/bigMath/bigDiv.js
+++ test/bigMath/bigDiv.js
@@ -1,7 +1,6 @@
 const BigNumber = require('bignumber.js');
 
 const bigMathArtifact = artifacts.require('BigMath');
-const truffleAssertions = require('truffle-assertions');
 
 contract('bigMath / bigDiv', () => {
   let contract;
@@ -9,93 +8,6 @@ contract('bigMath / bigDiv', () => {
 
   before(async () => {
     contract = await bigMathArtifact.new();
-  });
-
-  it('2x1 random math works', async () => {
-    for (let i = 0; i < 999; i++) {
-      const numA = new BigNumber(Math.random()).times(maxValue).dp(0);
-      const numB = new BigNumber(Math.random()).times(maxValue).dp(0);
-      const den = new BigNumber(Math.random()).times(maxValue).dp(0);
-      // console.log(`${numA.toFixed()}*${numB.toFixed()}/${den.toFixed()}`);
-      const expected = numA
-        .times(numB)
-        .div(den)
-        .dp(0);
-      if (expected.lte(maxValue)) {
-        const result = await contract.bigDiv2x1(
-          numA.toFixed(),
-          numB.toFixed(),
-          den.toFixed(),
-          false,
-        );
-        if (expected.gt(0)) {
-          const delta = expected.minus(result);
-          if (expected.gt(10000)) {
-            assert(delta.div(expected).lte(0.001));
-          } else {
-            // not very accurate when the result is a small number
-            assert(delta.lte(expected.div(2).dp(0, BigNumber.ROUND_UP)));
-          }
-        } else {
-          assert.equal(result, 0);
-        }
-      } else {
-        // revert if the result is > 256bits
-        await truffleAssertions.fails(
-          contract.bigDiv2x1(
-            numA.toFixed(),
-            numB.toFixed(),
-            den.toFixed(),
-            false,
-          ),
-        );
-      }
-    }
-  });
-
-  it('2x2 random math works', async () => {
-    for (let i = 0; i < 999; i++) {
-      const numA = new BigNumber(Math.random()).times(maxValue).dp(0);
-      const numB = new BigNumber(Math.random()).times(maxValue).dp(0);
-      const denA = new BigNumber(Math.random()).times(maxValue).dp(0);
-      const denB = new BigNumber(Math.random()).times(maxValue).dp(0);
-      // console.log(
-      //   `${numA.toFixed()}*${numB.toFixed()}/(${denA.toFixed()}*${denB.toFixed()})`
-      // );
-      const expected = numA
-        .times(numB)
-        .div(denA.times(denB))
-        .dp(0);
-      if (expected.lte(maxValue)) {
-        const result = await contract.bigDiv2x2(
-          numA.toFixed(),
-          numB.toFixed(),
-          denA.toFixed(),
-          denB.toFixed(),
-        );
-        if (expected.gt(0)) {
-          const delta = expected.minus(result);
-          if (expected.gt(10000)) {
-            assert(delta.div(expected).lte(0.001));
-          } else {
-            // not very accurate when the result is a small number
-            assert(delta.lte(expected.div(2).dp(0, BigNumber.ROUND_UP)));
-          }
-        } else {
-          assert.equal(result, 0);
-        }
-      } else {
-        // revert if the result is > 256bits
-        await truffleAssertions.fails(
-          contract.bigDiv2x2(
-            numA.toFixed(),
-            numB.toFixed(),
-            denA.toFixed(),
-            denB.toFixed(),
-          ),
-        );
-      }
-    }
   });
 
   it('2x1 does not overflow with sqrt max value', async () => {
